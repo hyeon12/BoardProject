b2671f75b320634ee87f99373481ca520c07b958
hyeon12 <seojh912_@naver.com>
2024-08-02 12:16:04 +0900

    파일 컨트롤러 테스트

--- src/main/java/org/hyeon/file/controllers/FileController.java
+++ src/main/java/org/hyeon/file/controllers/FileController.java
@@ -1,18 +1,35 @@
 package org.hyeon.file.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.hyeon.file.entities.FileInfo;
+import org.hyeon.file.services.FileUploadService;
+import org.hyeon.global.exceptions.RestExceptionProcessor;
+import org.hyeon.global.rests.JSONData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/file")
-public class FileController {
+@RequiredArgsConstructor
+public class FileController implements RestExceptionProcessor {
+
+    private final FileUploadService uploadService;
 
     @PostMapping("upload")
-    public void upload(@RequestPart("file") MultipartFile[] files,
-                       @RequestParam(name="gid", required = false) String gid,
-                       @RequestParam(name="location", required = false) String location){
+    public ResponseEntity<JSONData> upload(@RequestPart("file") MultipartFile[] files,
+                                           @RequestParam(name="gid", required = false) String gid,
+                                           @RequestParam(name="location", required = false) String location){
 
+        List<FileInfo> items = uploadService.upload(files, gid, location);
+        HttpStatus status = HttpStatus.CREATED;
+        JSONData data = new JSONData(items);
+        data.setStatus(status);
 
+        return ResponseEntity.status(status).body(data);
     }
 
 }
