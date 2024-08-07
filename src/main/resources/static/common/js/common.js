93901a16c62fea968094896fb7982a4bb104a5fb
hyeon12 <seojh912_@naver.com>
2024-08-07 15:50:50 +0900

    파일 공통 모듈

--- src/main/resources/static/common/js/common.js
+++ src/main/resources/static/common/js/common.js
@@ -1,29 +1,27 @@
 const commonLib = {
-    /*
+    /**
     * ajax 요청 공통 기능
     *
-    * @param responseType : 응답 데이터 타입(text - text로, 그 외는 json)
+    * @param responseType : 응답 데이터 타입(text - text로, 그외는 json)
     */
     ajaxLoad(url, method = "GET", data, headers, responseType) {
         if (!url) {
-            return; // url 없는 경우 보내지 X
+            return;
         }
 
         const csrfToken = document.querySelector("meta[name='csrf_token']")?.content?.trim();
-        // ?. - optional 체이닝 : null 이거나 undefined 이면 그냥 끝내도록! (안전하게 사용 가능)
         const csrfHeader = document.querySelector("meta[name='csrf_header']")?.content?.trim();
-        const rootUrl = document.querySelector("meta[name='rootUrl']")?.content?.trim() ?? '';
-
+        let rootUrl = document.querySelector("meta[name='rootUrl']")?.content?.trim() ?? '';
         rootUrl = rootUrl === '/' ? '' : rootUrl;
 
         url = location.protocol + "//" + location.host + rootUrl + url;
-        console.log(url);
+
         method = method.toUpperCase();
         if (method === 'GET') {
             data = null;
         }
 
-        if (!data instanceof FormData && typeof data !== 'string' && data instanceof Object){
+        if (data && !(data instanceof FormData) && typeof data !== 'string' && data instanceof Object) {
             data = JSON.stringify(data);
         }
 
@@ -36,14 +34,14 @@ const commonLib = {
             method
         };
 
-        if(data) options.body = data;
-        if(headers) options.headers = headers;
+        if (data) options.body = data;
+        if (headers) options.headers = headers;
 
         return new Promise((resolve, reject) => {
-        fetch(url, options)
-            .then(res => responseType === 'text' ? res.text() : res.json()) // res.json() - JSON / res.text() - 텍스트
-            .then(data => resolve(data))
-            .catch(err => reject(err));
+            fetch(url, options)
+                .then(res => responseType === 'text' ? res.text() : res.json()) // res.json() - JSON / res.text() - 텍스트
+                .then(data => resolve(data))
+                .catch(err => reject(err));
         });
     }
 };
